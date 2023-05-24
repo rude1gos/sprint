9a5f4fa43114dde2c60d1dc482ad6de1fce9cd62
rude1gos <117449228+rude1gos@users.noreply.github.com>
2023-05-24 17:25:35 +0500

    документация с помощью swagger и тесты методов класса БД
    документация с помощью swagger и тесты методов класса БД

--- pereval/tests.py
+++ pereval/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import PerevalAdded, Users, Coords, PerevalImages
+
+
+class PerevalAddedTests(APITestCase):
+    def setUp(self) -> None:
+        self.url = reverse('submit_data')
+        self.user = Users.objects.create(first_name='TestUser', last_name='TestUser', password='password',
+                                         email='email', otc='TestUser')
+        self.coord = Coords.objects.create(latitude=1.0, longitude=2.0, height=3.0)
+        self.img = PerevalImages.objects.create(img='image.jpg', image_name='img')
+        self.valid_payload = {
+            'user': self.user.pk,
+            'beauty_title': 'Test Beauty Title',
+            'title': 'Test Title',
+            'other_title': 'Test Other Title',
+            'connect': 'Test Connect',
+            'winter_level': 'Test Winter Level',
+            'summer_level': 'Test Summer Level',
+            'autumn_level': 'Test Autumn Level',
+            'spring_level': 'Test Spring Level',
+            'coord_id': self.coord.pk,
+            'images': [self.img.pk],
+            'status': 'new'
+        }
+        self.invalid_payload = {
+            'user': '',
+            'beauty_title': '',
+            'title': '',
+            'other_titles': '',
+            'connect': '',
+            'winter_level': '',
+            'summer_level': '',
+            'autumn_level': '',
+            'spring_level': '',
+            'coord_id': '',
+            'images': [],
+            'status': ''
+        }
+
+    def test_create_pereval_added_with_valid_payload(self):
+        data = self.valid_payload
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(PerevalAdded.objects.count(), 1)
+        self.assertEqual(PerevalAdded.objects.get().beauty_title, 'Test Beauty Title')
+
+    def test_create_pereval_added_with_invalid_payload(self):
+        data = self.invalid_payload
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(PerevalAdded.objects.count(), 0)
